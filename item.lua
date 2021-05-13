18cfd7523bb6e4ac70fa8de62332a0d62b604674
Markus Hanel <markus.hanel0@gmail.com>
2021-05-13 16:47:23 +0200

    pet gear

--- item.lua
+++ item.lua
@@ -1,4 +1,3 @@
-local lfs = require('lfs')
 local ldir = require('dir')
 local proto = require('proto')
 local lookup = require('lookup')
@@ -9,7 +8,7 @@ local paramsMeta = {
         rawset(self, key, value)
     end,
 }
-function getParams(id, platform, data)
+function getParamsHeroGear(id, platform, data)
     local params = setmetatable({}, paramsMeta)
     local paramsFragment = setmetatable({}, paramsMeta)
 
@@ -120,7 +119,7 @@ function getParams(id, platform, data)
     return params, paramsFragment
 end
 
-function generateGear(dest, id, data)
+function generateHeroGear(dest, id, data)
     local output = {}
     local outputFragment = {}
     local ndata = select(2, next(data))
@@ -137,7 +136,7 @@ function generateGear(dest, id, data)
             local data = data[platform]
 
             if data then
-                local params, paramsFragment = getParams(id, platform, data)
+                local params, paramsFragment = getParamsHeroGear(id, platform, data)
                 local data = {}
                 local dataFragment = {}
                 local prefix = ' |' .. platform .. '_'
@@ -194,7 +193,116 @@ function generateGear(dest, id, data)
     end
 end
 
-function generate(dest, lookup, src_browser, src_mobile)
+local attribute_fix = setmetatable({
+    intelligence = 'patronagepower',
+    strength = 'skillpower',
+}, {
+    __index = function(self, key)
+        local resolve = rawget(self, key) or rawget(self, key:lower())
+
+        if resolve then
+            self[key] = resolve
+
+            return resolve
+        end
+
+        self[key] = key
+
+        return key
+    end,
+})
+
+function getParamsPetGear(id, platform, data)
+    local params = setmetatable({}, paramsMeta)
+
+    data:expand()
+
+    if data.PetLevel then
+        params.pet_level_required = tonumber(data.PetLevel[1])
+    end
+
+    local levels = data.Levels
+
+    if levels then
+        local lookup_param = lookup.param
+        local gold = levels.GoldCost[1]
+        local particle = levels.ConsumableCost[1]
+        local emeralds = levels.StarmoneyCost[1]
+
+        for attribute, v in pairs(levels.Stats) do
+            local param = lookup_param[attribute_fix[attribute]]
+            local value = v[1]
+
+            params[param .. '_min'] = tonumber(value[2])
+            params[param .. '_max'] = tonumber(value[#value])
+        end
+
+        params.upgrade_max_level = #gold
+        params.upgrade_chaos_particle = tonumber(particle[2])
+        params.upgrade_gold = tonumber(gold[2])
+        params.upgrade_emeralds = tonumber(emeralds[2])
+    end
+
+    if data.UnlockCost then
+        params.buy_chaos_particle = tonumber(data.UnlockCost[1][4])
+    end
+
+    if data.SellCost then
+        params.sell_chaos_particle = tonumber(data.SellCost[1][4])
+    end
+
+    return params
+end
+
+function generatePetGear(dest, id, data)
+    local output = {}
+    local ndata = select(2, next(data))
+
+    print(id, ndata.name)
+
+    local browser = data.browser
+    local mobile = data.mobile
+
+    if browser and mobile and browser.name ~= mobile.name then
+        error(string.format("Name doesn't match!\nBrowser:\t%s\nMobile:\t%s", browser.name, mobile.name))
+    else
+        for _, platform in ipairs{'browser', 'mobile'} do -- ensure order
+            local data = data[platform]
+
+            if data then
+                local params = getParamsPetGear(id, platform, data)
+                local data = {}
+                local prefix = ' |' .. platform .. '_'
+
+                for idx, key in ipairs(params) do
+                    data[idx] = string.format('%s%s = %s', prefix, key, params[key])
+                end
+
+                table.insert(output, table.concat(data, '\n'))
+            end
+        end
+    end
+
+    if #output > 0 then
+        table.insert(output, 1, '<onlyinclude>{{<includeonly>#invoke:Data|set</includeonly><noinclude>Infobox Item/Pet</noinclude>')
+        -- table.insert(output, 2, ' |id = ' .. id)
+        table.insert(output, 2, ' |color = ' .. ndata.color)
+        table.insert(output, '}}</onlyinclude>')
+
+        output = table.concat(output, '\n')
+
+        -- print(output)
+
+        local file = io.open(table.concat{dest, '/', ndata.name, '-', id, '.txt'}, 'w')
+
+        if file then
+            file:write(output)
+            file:close()
+        end
+    end
+end
+
+function generate(dest, func, lookup, src)
     local gears = setmetatable({}, {
         __index = function(self, key)
             local data = {}
@@ -204,12 +312,10 @@ function generate(dest, lookup, src_browser, src_mobile)
             return data
         end,
     })
-    for k, v in pairs(proto[src_browser].Id) do
-        gears[tonumber(k)].browser = true
-    end
-
-    for k, v in pairs(proto[src_mobile].Id) do
-        gears[tonumber(k)].mobile = true
+    for platform, v in pairs(src) do
+        for k in pairs(v) do
+            gears[tonumber(k)][platform] = true
+        end
     end
 
     for id, data in pairs(gears) do
@@ -220,16 +326,16 @@ function generate(dest, lookup, src_browser, src_mobile)
         if data.browser then
             if data.mobile then
                 if data.browser.name == data.mobile.name then
-                    generateGear(dest, id, data)
+                    func(dest, id, data)
                 else
-                    generateGear(dest, id, {browser = data.browser})
-                    generateGear(dest, id, {mobile = data.mobile})
+                    func(dest, id, {browser = data.browser})
+                    func(dest, id, {mobile = data.mobile})
                 end
             else
-                generateGear(dest, id, {browser = data.browser})
+                func(dest, id, {browser = data.browser})
             end
         else
-            generateGear(dest, id, {mobile = data.mobile})
+            func(dest, id, {mobile = data.mobile})
         end
     end
 end
@@ -237,9 +343,10 @@ end
 do
     local dest = 'dest/item'
 
-    lfs.mkdir('dest')
-    lfs.mkdir('dest/item')
+    ldir:mkdir(dest)
 
-    generate(dest, lookup.item.gear, 'src/Proto/Gear.proto', 'src/Mobile/Proto/Gear.proto')
-    generate(dest, lookup.item.scroll, 'src/Proto/Scroll.proto', 'src/Mobile/Proto/Scroll.proto')
+    generate(dest, generateHeroGear, lookup.item.gear, {browser = proto['src/Proto/Gear.proto'].Id, mobile = proto['src/Mobile/Proto/Gear.proto'].Id})
+    generate(dest, generateHeroGear, lookup.item.scroll, {browser = proto['src/Proto/Scroll.proto'].Id, mobile = proto['src/Mobile/Proto/Scroll.proto'].Id})
+
+    generate(dest, generatePetGear, lookup.item.petgear, {browser = proto['src/Proto/Items.proto']['Pet Gears'].Id})
 end
